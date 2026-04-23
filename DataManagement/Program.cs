1b9885439f6ea480641c4bd93fc8128f3f605c08
Lisa <kareevaliza@gmail.com>
2026-04-23 17:25:13 +0100

    scripted the creation of customers to the table

--- DataManagement/Program.cs
+++ DataManagement/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExamPrep2024_Booking;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,28 @@ namespace DataManagement
     {
         static void Main(string[] args)
         {
+            RestaurantData db = new RestaurantData();
+            try
+            {
+                using (db)
+                {
+                    Customer c1 = new Customer { Name = "John Doe", ContactNumber = "1234567890" };
+                    Customer c2 = new Customer { Name = "Jane Smith", ContactNumber = "0987654321" };
+                    Customer c3 = new Customer { Name = "Alice Johnson", ContactNumber = "5555555555" };
+
+                    Console.WriteLine("Created customers");
+
+                    db.Customers.Add(c1);
+                    db.Customers.Add(c2);
+                    db.Customers.Add(c3);
+
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
