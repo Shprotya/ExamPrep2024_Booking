18d6d40008c49a2499a20a966967102b9d70e24f
Lisa <kareevaliza@gmail.com>
2026-04-23 16:45:52 +0100

    created required classes

---
+++ ExamPrep2024_Booking/Booking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPrep2024_Booking
+{
+    internal class Booking
+    {
+    }
+}
